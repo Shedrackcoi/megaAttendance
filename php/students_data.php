b1dcbe62fafe3c9201530928ab6898f084ba2769
Unity Iyedoh <unityiyedoh@gmail.com>
2026-05-23 15:14:47 +0100

    i added my students details using php

--- php/students_data.php
+++ php/students_data.php
@@ -11,11 +11,6 @@ $students[] = [
     "department" => "Software Engineering"
 ];
 
-// DISPLAY THE DATA
-foreach ($students as $student) {
-    echo "Matric No: " . $student["matric_no"] . "<br>";
-    echo "Name: " . $student["name"] . "<br>";
-    echo "Department: " . $student["department"] . "<br><br>";
-}
 
-?>S
+
+?>
